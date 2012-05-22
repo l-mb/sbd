d403599e1a97d62c336e16c0b4ad81e70a6369e2
Lars Marowsky-Bree <lmb@suse.de>
2012-05-22 17:12:25 +0200

    Make DBGLOG macro compile.

--- sbd.h
+++ sbd.h
@@ -202,5 +202,5 @@ int pcmk_have_quorum(void);
 int servant_pcmk(const char *diskname, const void* argp);
 
 #define DBGLOG(lvl, fmt, args...) do { \
-	if (debug > 1) cl_log(lvl, fmt, args...) \
-	} while(0);
+	if (debug > 1) cl_log(lvl, fmt, ##args); \
+	} while(0)
